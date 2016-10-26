8fc9572c85f9481929d84379db1d9da28be93612
rubenfig <mangekyou1>
2016-10-26 15:04:07 -0300

    -Se agrego el service de la api de github

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the desarrolloAgilApp
  */
 angular.module('desarrolloAgilApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope,User) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.username="rubenfig";
+    $scope.password="mangekyou1";
+
+    //Aca se generea el token de autenticacion
+    var authToken = "Basic " + btoa($scope.username + ":" + $scope.password);
+
+    //Aca se hace get de los datos, ahi dentro del then hay que asignar la variable que recibimos
+    $scope.datos=User.get(authToken).query({'username': 'carlossegovia'}).$promise.then(function(data) {
+      console.log(data);
+    });
   });
---
+++ app/scripts/services/api.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name desarrolloAgilApp.Api
+ * @description
+ * # Api
+ * Service in the dagilApp.
+ */
+angular.module('desarrolloAgilApp')
+  .factory('CurrentUser', function ($resource) {
+    return {
+      get: function (token) {
+        return $resource('https://api.github.com/user',null,{
+          query: {
+            method: 'GET',
+            headers: {
+              'Authorization': token
+            }
+          }
+        })
+      }
+    }})
+  .factory('User', function ($resource) {
+     return {
+        get: function (token) {
+          return $resource('https://api.github.com/users/:username/',null,{
+            query: {
+              method: 'GET',
+              headers: {
+                'Authorization': token
+              }
+            }
+          })
+        }
+      }})
+  .factory('Repos', function ($resource) {
+    return {
+      get: function (token) {
+        return $resource('https://api.github.com/users/:username/repos/',null,{
+          query: {
+            method: 'GET',
+            headers: {
+              'Authorization': token
+            }
+          }
+        })
+      }
+    }
+  });
