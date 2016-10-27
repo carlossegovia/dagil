33a4318a3802f742fd2fe973f8cdf2bced4723c7
carlossegovia <karlucho_94_s@hotmail.com>
2016-10-27 09:59:48 -0300

    Se debe de crear un template para mostar los datos personales del usuario logueado y cambiar el location path de login para que se rediriga ahí

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the desarrolloAgilApp
  */
 angular.module('desarrolloAgilApp')
-  .controller('MainCtrl', function ($scope,User) {
+  .controller('MainCtrl', function ($scope, $location, User) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -25,6 +25,9 @@ angular.module('desarrolloAgilApp')
       //Aca se hace get de los datos, ahi dentro del then hay que asignar la variable que recibimos
       $scope.datos=User.get(authToken).query({'username': $scope.username}).$promise.then(function(data) {
         console.log(data);
+        $location.path('/about');
+      }, function (error) {
+        window.alert("Usuario o Contraseña incorrectos");
       });
     }
   });
